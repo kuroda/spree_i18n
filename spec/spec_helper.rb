195972e6814c2fb4aaa1b8eac6879930c738b782
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-05-03 09:50:05 +0900

    Add a matcher to check if all keys are translated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'i18n-spec'
 require 'i18n/core_ext/hash'
 require 'active_support/core_ext/kernel/reporting'
 require 'support/fake_app'
+require 'support/be_a_thorough_translation_of_matcher'
 
 RSpec.configure do |config|
   config.mock_with :rspec
