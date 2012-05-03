d60ed5f1351a71e7a51708b36006aac933b0f8d8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2012-05-03 10:27:43 +0900

    Add a matcher to check if all keys are translated

--- spec/support/be_a_thorough_translation_of_matcher.rb
+++ spec/support/be_a_thorough_translation_of_matcher.rb
@@ -4,7 +4,9 @@ RSpec::Matchers.define :be_a_thorough_translation_of do |default_locale_filepath
     default_locale = I18nSpec::LocaleFile.new(default_locale_filepath)
     
     @misses = default_locale.flattened_translations.select do |key, value|
-      !@keys.include?(key) && locale_file.flattened_translations[key] == value
+      !@keys.include?(key) &&
+        locale_file.flattened_translations[key] != "" &&
+        locale_file.flattened_translations[key] == value
     end
     @misses.empty?
   end
